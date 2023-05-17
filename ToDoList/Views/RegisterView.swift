e6065441599155eb0d90936ddcd5de8082407864
Vladisalv Moiseev <80466128+Moisey-mav@users.noreply.github.com>
2023-05-17 15:09:59 +0300

    Сontrol storage

--- ToDoList/Views/RegisterView.swift
+++ ToDoList/Views/RegisterView.swift
@@ -8,10 +8,33 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         VStack {
             // Header
             HeaderView(title: "Register", suptitle: "Start organizing tods", angle: -15, background: .yellow)
+            
+            Form {
+                TextField("Full Name", text: $name)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocorrectionDisabled()
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled()
+                TextField("Password", text: $password)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                
+                TLButton(title: "Create Account", background: .green) {
+                    // Attempt registration
+                }
+                .padding()
+            }
+            .offset(y: -50)
+            
             Spacer()
         }
     }
